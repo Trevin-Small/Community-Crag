03f6b1702723c169ec6008e98a90e45c27c5670f
Trevin Small <trevincub03@gmail.com>
2022-03-01 21:27:31 -0500

    Fixed icon size, added captcha v3

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { initializeAppCheck } from 'firebase/app-check';
 import { getFirestore, collection } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
@@ -18,6 +19,16 @@ const firebaseConfig = {
 
 // Firebase initialization
 const app = initializeApp(firebaseConfig);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Lc7paQeAAAAACamJ2DczHj2aui9LoDeJgGNFuoq'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
 
 // <-------------------------------------------------------------------------------------->
 
